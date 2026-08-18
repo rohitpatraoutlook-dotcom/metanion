83b4d6cb314d179800c18c7dafc4bdb09fae113b
Rohit Patra <Rohitkumarpatra42@gmail.com>
2026-08-19 02:43:18 +0530

    Version 2.0.1: Updated README with complete documentation

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="metanion",
-    version="2.0.0",
+    version="2.0.1",
     author="Rohit Patra",
     author_email="rohitpatra@outlook.com",
     description="A zero-weight symbolic tensor engine",
